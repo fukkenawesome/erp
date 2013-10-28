e8c9d0465052e39585f6ad2d87b45bb26ae275d6
electr0Nik <Zkorpio@freenet.de>
2013-10-28 21:40:29 +0100

    erste lauffähige version

--- src/main/java/de/kuebler/erp/crm/dao/impl/IAmSuperDaoImpl.java
+++ src/main/java/de/kuebler/erp/crm/dao/impl/IAmSuperDaoImpl.java
@@ -1,11 +1,9 @@
 package de.kuebler.erp.crm.dao.impl;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
-public class IAmSuperDaoImpl{
+public class IAmSuperDaoImpl extends MyTestDaoNamedParameterJdbcTemplate{
 
 	/*
 	@Autowired
--- src/main/java/de/kuebler/erp/crm/dao/impl/MyTestDaoNamedParameterJdbcTemplate.java
+++ src/main/java/de/kuebler/erp/crm/dao/impl/MyTestDaoNamedParameterJdbcTemplate.java
@@ -1,106 +1,69 @@
 package de.kuebler.erp.crm.dao.impl;
 
-import java.sql.ResultSet;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
+import javax.annotation.PostConstruct;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcDaoSupport;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.stereotype.Repository;
 
-import de.kuebler.erp.crm.dao.RequestsDAO;
-import de.kuebler.erp.crm.mvc.model.RequestModel;
+public class MyTestDaoNamedParameterJdbcTemplate extends
+		NamedParameterJdbcDaoSupport {
 
-@Repository
-public class MyTestDaoNamedParameterJdbcTemplate extends NamedParameterJdbcDaoSupport{
-
-	
-	@Autowired
-	 private DataSource dataSource;
-
-	 @PostConstruct
-	 public void doSomething() {
-	   super.setDataSource(dataSource);
-	 // test: 
-	@SuppressWarnings("deprecation")
-	long success = getJdbcTemplate().queryForLong("SELECT * FROM dual");
-	System.out.println("success: " + (success>0) );
-	 }
-	/*
 	@Autowired
 	private DataSource dataSource;
-	private NamedParameterJdbcTemplate jdbcTemplateObject;
-	
-	
-	public MyTestDaoNamedParameterJdbcTemplate(DataSource dataSource) {
-		//super(dataSource);
-		this.dataSource = dataSource;
-	}
-	
-	private final String TEST_QUERY = "select * from dual";
-	public void success(){
-		Map<String, String> param = new HashMap<String, String>();
-		int returnValue = jdbcTemplateObject.queryForObject(TEST_QUERY, param, Integer.class);
-		if(returnValue > 0)
-			System.out.println("WOHOOO");
-	}
-	
-	
-	public void setDataSource(DataSource dataSource){
-		this.dataSource = dataSource;
-		 this.jdbcTemplateObject = new NamedParameterJdbcTemplate(dataSource);
-	}
-	
-	public DataSource getDataSource(){
-		return this.dataSource;
-	}
-	
-	public NamedParameterJdbcTemplate getTemplate(){
-		return this.jdbcTemplateObject;
-	}
 
-
-	public List<RequestModel> getAllActiveRequests() {
-		// TODO Auto-generated method stub
-		return null;
+	@PostConstruct
+	public void init() {
+		super.setDataSource(dataSource);
+		long success = getJdbcTemplate().queryForLong("SELECT * FROM dual");
+		System.out.println("success: " + (success > 0));
 	}
-
-
-	public ResultSet getRequestsNew() {
-		success();
-		return null;
-	}
-
-
-	public ResultSet getRequestsInProcess() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-
-	public ResultSet getRequestsProcessed() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-
-	public ResultSet getAllInactiveRequests() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-
-	public ResultSet getRequest(Integer Id) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	
-	public void doSomethingDatabase(){
-		success();
-	}
-	*/
+	/*
+	 * @Autowired private DataSource dataSource; private
+	 * NamedParameterJdbcTemplate jdbcTemplateObject;
+	 * 
+	 * 
+	 * public MyTestDaoNamedParameterJdbcTemplate(DataSource dataSource) {
+	 * //super(dataSource); this.dataSource = dataSource; }
+	 * 
+	 * private final String TEST_QUERY = "select * from dual"; public void
+	 * success(){ Map<String, String> param = new HashMap<String, String>(); int
+	 * returnValue = jdbcTemplateObject.queryForObject(TEST_QUERY, param,
+	 * Integer.class); if(returnValue > 0) System.out.println("WOHOOO"); }
+	 * 
+	 * 
+	 * public void setDataSource(DataSource dataSource){ this.dataSource =
+	 * dataSource; this.jdbcTemplateObject = new
+	 * NamedParameterJdbcTemplate(dataSource); }
+	 * 
+	 * public DataSource getDataSource(){ return this.dataSource; }
+	 * 
+	 * public NamedParameterJdbcTemplate getTemplate(){ return
+	 * this.jdbcTemplateObject; }
+	 * 
+	 * 
+	 * public List<RequestModel> getAllActiveRequests() { // TODO Auto-generated
+	 * method stub return null; }
+	 * 
+	 * 
+	 * public ResultSet getRequestsNew() { success(); return null; }
+	 * 
+	 * 
+	 * public ResultSet getRequestsInProcess() { // TODO Auto-generated method
+	 * stub return null; }
+	 * 
+	 * 
+	 * public ResultSet getRequestsProcessed() { // TODO Auto-generated method
+	 * stub return null; }
+	 * 
+	 * 
+	 * public ResultSet getAllInactiveRequests() { // TODO Auto-generated method
+	 * stub return null; }
+	 * 
+	 * 
+	 * public ResultSet getRequest(Integer Id) { // TODO Auto-generated method
+	 * stub return null; }
+	 * 
+	 * public void doSomethingDatabase(){ success(); }
+	 */
 }
